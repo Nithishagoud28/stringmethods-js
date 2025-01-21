e871d63543dcb26ab9dbbacc1255094180aa6bc2
Nithishagoud28 <nithishagoud45@gmail.com>
2025-01-21 15:01:00 +0530

    string methods

---
+++ index.js
@@ -0,0 +1,7 @@
+let arr1=["html","css","JAVASCRIPT","react","ANGULAR"];
+arr1[0]=arr1[0].toUpperCase()
+arr1[1]=arr1[1].toUpperCase()
+arr1[2]=arr1[2].toLowerCase()
+arr1[3]=arr1[3].toUpperCase()
+arr1[4]=arr1[4].toLowerCase()
+console.log(arr1)
